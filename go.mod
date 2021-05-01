e331ae069652a799407a37392de6df1a05f68aa0
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-01 00:15:42 -0300

    feat: create keycloak client

---
+++ go.mod
@@ -0,0 +1,10 @@
+module goclient
+
+go 1.16
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible // indirect
+	github.com/pquerna/cachecontrol v0.1.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210427180440-81ed05c6b58c // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
---
+++ goclient/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientID     = "myclient"
+	clientSecret = "93ffdc85-f980-4d8b-b986-eaa0f741556d"
+)
+
+func main() {
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/myrealm")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state := "123"
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
